f22828ad840a2f8ca38b8b0a044e204b861dce40
Maddayum22 <madvanslooten@gmail.com>
2023-06-12 13:56:36 +0200

    latest gitlab code

--- backend-main/backend-main/src/data/models/post-interface.ts
+++
@@ -1,6 +0,0 @@
-import { PostBusiness, PostInterface } from '../../business/models/posts';
-
-export interface PostRepositoryInterface {
-    create(post: PostInterface): Promise<boolean>;
-    findAll(): Promise<PostInterface[]>;
-}
